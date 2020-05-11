27a4a46f82d3c4aee2c03f98cb2c3ede84de5b13
Benjamin Lupton <b@lupton.cc>
2020-05-11 20:58:16 +1000

    v1.8.0 - boundation, changelog, version bump

--- source/test.ts
+++ source/test.ts
@@ -1,8 +1,8 @@
 import kava from 'kava'
 import startOfWeek from './'
 
-kava.suite('start-of-week', function(suite, test) {
-	test('monday or sunday', function() {
+kava.suite('start-of-week', function (suite, test) {
+	test('monday or sunday', function () {
 		const result = startOfWeek
 		console.log(result)
 		if (result !== 0 && result !== 1) {
